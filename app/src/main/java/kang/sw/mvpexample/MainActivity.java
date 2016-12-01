b1475fa4a79156c0a395a3fb340fe77e91988ba6
ksu3101 <burkdog@naver.com>
2016-12-01 21:23:53 +0900

    [add] util class

--- app/src/main/java/kang/sw/mvpexample/MainActivity.java
+++ app/src/main/java/kang/sw/mvpexample/MainActivity.java
@@ -3,6 +3,7 @@ package kang.sw.mvpexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity
     extends AppCompatActivity {
 
---
+++ app/src/main/java/kang/sw/mvpexample/MainFragment.java
@@ -0,0 +1,9 @@
+package kang.sw.mvpexample;
+
+/**
+ * @author KangSungWoo
+ * @since 2016-12-01
+ */
+
+public class MainFragment {
+}
