1f057181aa7fdca5f23e1e5afab63a087b223f9f
ksu3101 <burkdog@naver.com>
2016-12-20 23:28:34 +0900

    [fix] log method

--- app/src/main/java/kang/sw/mvpexample/utils/common/SwLog.java
+++ app/src/main/java/kang/sw/mvpexample/utils/common/SwLog.java
@@ -40,15 +40,45 @@ public class SwLog {
   }
 
   public static void d(@NonNull String tag, @Nullable String msg) {
-    Log.d(tag, msg != null ? msg : "Message is Null.");
+    if (msg != null) {
+      for (int i = 0; i <= msg.length() / LOG_DEF_LENGTH; i++) {
+        int start = i * LOG_DEF_LENGTH;
+        int end = (i + 1) * LOG_DEF_LENGTH;
+        end = (end > msg.length() ? msg.length() : end);
+        Log.d(tag + "[" + i + "] ", msg.substring(start, end));
+      }
+    }
+    else {
+      Log.d(tag, "Message is Null.");
+    }
   }
 
   public static void w(@NonNull String tag, @Nullable String msg) {
-    Log.w(tag, msg != null ? msg : "Message is Null.");
+    if (msg != null) {
+      for (int i = 0; i <= msg.length() / LOG_DEF_LENGTH; i++) {
+        int start = i * LOG_DEF_LENGTH;
+        int end = (i + 1) * LOG_DEF_LENGTH;
+        end = (end > msg.length() ? msg.length() : end);
+        Log.w(tag + "[" + i + "] ", msg.substring(start, end));
+      }
+    }
+    else {
+      Log.w(tag, "Message is Null.");
+    }
   }
 
   public static void e(@NonNull String tag, @Nullable String msg) {
-    Log.e(tag, msg != null ? msg : "Message is Null.");
+    if (msg != null) {
+      for (int i = 0; i <= msg.length() / LOG_DEF_LENGTH; i++) {
+        int start = i * LOG_DEF_LENGTH;
+        int end = (i + 1) * LOG_DEF_LENGTH;
+        end = (end > msg.length() ? msg.length() : end);
+        Log.e(tag + "[" + i + "] ", msg.substring(start, end));
+      }
+    }
+    else {
+      Log.e(tag, "Message is Null.");
+    }
   }
 
 }
